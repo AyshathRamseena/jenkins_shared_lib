57dd28e7d554d4d3e53d5a120ad52e008e4d869f
AyshathRamseena <147328013+AyshathRamseena@users.noreply.github.com>
2023-11-29 18:36:38 +0400

    Update jfrogPush.groovy

--- vars/jfrogPush.groovy
+++ vars/jfrogPush.groovy
@@ -1,3 +1,4 @@
 def call(){
-  sh """curl-X PUT-u admin -T /var/lib/jenkins/workspace/jfrogPush/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://35.174.211.196/artifactory/example-repo-local/"""  
+  sh "chmod +x -R ./jfrog.py"
+  sh './jfrog.py'
 }
